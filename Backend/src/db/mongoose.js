c727ee7e22e2b5fa24b30fd79192719f19514433
arjuna5194 <68380997+arjuna5194@users.noreply.github.com>
2024-06-01 12:14:21 +1000

    Update mongoose.js

--- Backend/src/db/mongoose.js
+++ Backend/src/db/mongoose.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
 await mongoose.connect(
-  "mongodb+srv://ajeesdonga:ajeesdonga@cluster0.lds9zzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://ajeesdonga:ajeesdonga@cluster0.lds9zzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
 );
